2e0bdbd5c5b0b7832c4315284fd7341fbc24b49d
Roland Chelwing <jolleee@hotmail.com>
2024-05-31 21:30:16 +0200

    Randomize prompts

--- components/prompts.tsx
+++ components/prompts.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { FlipWords } from "./ui/flip-words";
+import { shuffle } from "@/utils/array";
 
 const regularVotes = ["single-choice polls", "multi-choice polls", "storypoints voting"];
 const Prompts = () => (
   <div className="text-2xl mx-auto font-normal text-neutral-800 dark:text-neutral-400">
-    Create <FlipWords words={regularVotes} /> easier than deciding on what pizza to get
+    Create <FlipWords words={shuffle(regularVotes)} /> easier than deciding on what pizza to get
   </div>
 );
 
