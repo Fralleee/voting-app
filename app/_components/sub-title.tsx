9321ab0d9dfee83fc2d062dc02fc9ab2e4d2def3
Roland Chelwing <jolleee@hotmail.com>
2024-06-10 16:57:15 +0200

    Fix text color

--- app/_components/sub-title.tsx
+++ app/_components/sub-title.tsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 export const SubTitle = ({ children }: { children: React.ReactNode }) => {
   return (
     <motion.h1
-      className="relative text-center text-xl font-bold text-muted md:text-3xl lg:text-4xl"
+      className="relative text-center text-xl font-bold text-muted-foreground md:text-3xl lg:text-4xl"
       initial={{ scale: 0.95, y: -20, opacity: 0 }}
       animate={{ scale: 1, y: 0, opacity: 1 }}
       transition={{ ease: "easeOut", duration: 0.3 }}
