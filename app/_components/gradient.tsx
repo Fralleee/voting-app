b7c9c494fff0e3d6fa89f3f0e18fed727d0b721d
Roland Chelwing <jolleee@hotmail.com>
2024-06-12 20:43:53 +0200

    Fix gradient colors

--- app/_components/gradient.tsx
+++ app/_components/gradient.tsx
@@ -10,10 +10,10 @@ export const Gradient = () => {
       animate={{ opacity: 1 }}
       transition={{ ease: "easeOut", duration: 0.4 }}
     >
-      <div className="absolute inset-x-0 top-0 h-[2px] w-full bg-gradient-to-r from-transparent via-purple-400 to-transparent blur-sm dark:via-indigo-200" />
-      <div className="absolute inset-x-0 top-0 h-px w-full bg-gradient-to-r from-transparent via-purple-400 to-transparent dark:via-indigo-200" />
-      <div className="absolute left-1/4 right-1/4 top-0 h-1 w-1/2 bg-gradient-to-r from-transparent via-pink-400 to-transparent blur-sm dark:h-[5px] dark:via-sky-400" />
-      <div className="absolute left-1/4 right-1/4 top-0 h-px w-1/2 bg-gradient-to-r from-transparent via-pink-400 to-transparent dark:via-sky-400" />
+      <div className="absolute inset-x-0 top-0 h-[2px] w-full bg-gradient-to-r from-transparent via-pink-400 to-transparent blur-sm dark:via-indigo-200" />
+      <div className="absolute inset-x-0 top-0 h-px w-full bg-gradient-to-r from-transparent via-pink-400 to-transparent dark:via-indigo-200" />
+      <div className="absolute left-1/4 right-1/4 top-0 h-1 w-1/2 bg-gradient-to-r from-transparent via-red-400 to-transparent blur-sm dark:h-[5px] dark:via-sky-400" />
+      <div className="absolute left-1/4 right-1/4 top-0 h-px w-1/2 bg-gradient-to-r from-transparent via-red-400 to-transparent dark:via-sky-400" />
     </motion.div>
   );
 };
