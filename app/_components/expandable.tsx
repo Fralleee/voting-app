1bf84d1117085c305dd7dbf309dd95b46c5eb0a1
Roland Chelwing <jolleee@hotmail.com>
2024-06-12 21:20:52 +0200

    Another fix

--- app/_components/expandable.tsx
+++ app/_components/expandable.tsx
@@ -8,7 +8,7 @@ interface ExpandableProps {
 
 export const Expandable = ({ expanded, children }: ExpandableProps) => {
   const contentRef = useRef<HTMLDivElement>(null);
-  const [maxHeight, setMaxHeight] = useState(500);
+  const [maxHeight, setMaxHeight] = useState(expanded ? 500 : 0);
 
   useEffect(() => {
     if (expanded) {
