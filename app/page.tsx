94f5b1b3b579ba2d8a976a39bb9b910ef7fab639
Roland Chelwing <jolleee@hotmail.com>
2024-06-23 00:21:50 +0200

    Better responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export const dynamic = "force-dynamic";
 export default function Page() {
   const title = getRandomTitle();
   return (
-    <div className="flex w-full flex-grow flex-col items-center justify-center gap-3">
+    <div className="flex h-full w-full flex-col items-center gap-6 py-12 pt-20 md:h-auto md:px-8 md:pb-64">
       <Title>{title}</Title>
       <Gradient />
       <PollButton />
